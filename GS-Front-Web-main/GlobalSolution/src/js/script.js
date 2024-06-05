19406d6b5340ee81d5168ac28f7c83a8193d2477
Felipe <menfelipe1@gmail.com>
2024-06-05 17:47:54 -0300

    adicionando slide show

--- GS-Front-Web-main/GlobalSolution/src/js/script.js
+++ GS-Front-Web-main/GlobalSolution/src/js/script.js
@@ -6,3 +6,31 @@ function mudarCor() {
     document.body.style.backgroundColor = isColor1 ? color1 : color2;
     isColor1 = !isColor1;
 }
+
+let imgs_problema =['src/img/praia-poluida.jpg','src/img/praia-poluida2.jpg','src/img/praia-poluida3.jpg'];
+let index =0;
+let time =3000;
+
+function slideShow(){
+    document.getElementById('img-problema').src=imgs_problema[index];
+    index++;
+
+        if(index == imgs_problema.length){
+          index =0;
+        }
+        setTimeout('slideShow()',time);
+}
+slideShow();
+
+let imgs_tecnologia =['src/img/tecnologia1.jpg','src/img/tecnologia2.jpg','src/img/tecnologia3.jpg'];
+
+function slideShow2(){
+    document.getElementById('img-tec').src=imgs_tecnologia[index];
+    index++;
+
+        if(index == imgs_tecnologia.length){
+          index =0;
+        }
+        setTimeout('slideShow2()',time);
+}
+slideShow2();
